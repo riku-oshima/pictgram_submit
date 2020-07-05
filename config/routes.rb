bfffb16358cc9fb567ad605e24093c4cdb771008
EC2 Default User <ec2-user@ip-172-31-40-209.us-east-2.compute.internal>
2020-07-05 06:18:50 +0000

    submit-homework

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'users/new'
+  get 'user/new'
+  get 'pages/help'
+root 'pages#index'
+
+  resources :users
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+end
+
