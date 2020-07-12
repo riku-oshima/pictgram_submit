40b2bfcf8dd94162f51233cd3f97c424415806da
riku-oshima <67011972+riku-oshima@users.noreply.github.com>
2020-07-12 15:53:00 +0900

    Update topics_controller.rb

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
   end
   
   def create
-    @topic = current_user.topic.new(topic_params)
+    @topic = current_user.topics.new(topic_params)
     
     if @topic.save
       redirect_to topics_path, success:'投稿に成功しました'
